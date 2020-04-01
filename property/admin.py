7dfe771392664bc75e4132a83c6693f5658cafff
danceandfight <danceandfight@gmail.com>
2020-04-01 20:51:59 +0300

    first commit

---
+++ property/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Flat
+
+class FlatAdmin(admin.ModelAdmin):
+    search_fields = ('town', 'address', 'owner')
+
+admin.site.register(Flat, FlatAdmin)
+#admin.site.register(Flat)
+
+
